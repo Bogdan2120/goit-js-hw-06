151b9c9bb4489824f56d696b648f5fce27d676b0
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2022-11-06 18:15:32 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const ref = {
+  btnEl: document.querySelector('.change-color'),
+  spanEl: document.querySelector('.color'),
+  bodyEL: document.body,
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onСhangeColorBody = e => {
+  ref.bodyEL.style.backgroundColor = `${getRandomHexColor()}`;
+  ref.spanEl.textContent = `${getRandomHexColor()}`;
+};
+
+ref.btnEl.addEventListener('click', onСhangeColorBody);
